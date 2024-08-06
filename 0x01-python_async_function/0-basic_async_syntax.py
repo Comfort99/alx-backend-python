959cb06a9ae435b3c3492b7e37a8e0bee8b6fba8
Comfort99 <shingangecomfort@gmail.com>
2024-08-06 11:05:23 +0200

    asynchronous coroutine

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(mex_delay: int = 10) -> str:
+async def wait_random(mex_delay: int = 10) -> float:
     """ coroutine that takes in an integer argument
       Waits for a random delay between 0 and max_delay
       seconds and eventually returns it
