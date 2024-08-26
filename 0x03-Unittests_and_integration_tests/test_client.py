9b498d0c7cb7038680ab1d1b01e40b36085310de
Comfort99 <shingangecomfort@gmail.com>
2024-08-26 20:51:46 +0200

    PropertyMock

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 from client import GithubOrgClient
 from parameterized import parameterized
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -16,8 +16,24 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch('client.get_json')
     def test_org(self, org_name, mock_get_json):
-        """ A method that test Org method and return a json """
+        """ A method that test Org method and
+        return a value of passed name_url"""
         test_org = GithubOrgClient(org_name)
-        test_org.org()
+        url = test_org.org()
+        print(url)
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}")
+
+    def test_public_repos_url(self):
+        """ an org property that returns a value of a key """
+
+        # patch the org property of GithubOrgClient
+        with patch.object(GithubOrgClient,
+                          'org', new_callable=PropertyMock) as mock_org:
+            expected_url = "https://api.github.com/orgs/test_org/repos"
+            payload = {"repos_url": expected_url}
+            mock_org.return_value = payload
+            test_repos = GithubOrgClient('test_org')
+            result = test_repos._public_repos_url
+            print(result)
+            self.assertEqual(result, expected_url)
